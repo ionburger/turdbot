ccebb647f940860922dd57cecb9f80cf8fb7199b
ionburger <ionburgger@gmail.com>
2022-10-29 12:32:37 -0600

    eee

--- py/init.py
+++ py/init.py
@@ -1,5 +1,9 @@
 import version
+import sys
+import os
 if version.local() < version.remote():
     print("needs update")
 else:
     print("is fine")
+if input("aaa") == "y":
+    os.execl(sys.executable, sys.executable, *sys.argv)
