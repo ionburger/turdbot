71a508b99542ea3e3fc17118acb82af56675d2e5
ionburger <ionburgger@gmail.com>
2022-11-15 19:28:22 -0700

    eeee

---
+++ py/bot.py
@@ -0,0 +1,40 @@
+#imports
+import bin.version as version
+import modules.main as main
+import sys
+import os
+import configparser
+import wget
+import shutil
+
+#funky stuff to deal with relative file paths
+randir = os.getcwd()
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+update = False
+config = configparser.ConfigParser()
+config.read("config/config.conf")
+if config["config"]["autoupdate"] == "true":
+    versionl = version.local()
+    versionr = version.remote()
+    if ".".join(versionl)<".".join(versionr):
+        print("outdated version",versionl,"<",versionr)
+        if config["config"]["autoupdate"] == "true":
+            print("autoupdate is enabled, attempting to update")
+            wget.download("https://github.com/ionburger/turdbot/archive/refs/tags/"+versionr+".zip",out = "turdbottmp.zip")
+            shutil.unpack_archive("turdbottmp.zip","turdbottmp")
+            for file in os.listdir("turdbottmp/turdbot-"+versionr+"/py/"):
+                if file.endswith(".py"):
+                    shutil.move("turdbottmp/turdbot-"+versionr+"/py/"+file,file)
+            with open("VERSION","w") as file:
+                file.write(versionr)
+            shutil.rmtree("turdbottmp")
+            os.remove("turdbottmp.zip")
+            os.chdir(randir)
+            os.execl(sys.executable, sys.executable, *sys.argv)
+            update = True
+    else:
+        print("running latest version of turdbot",versionl)
+else:
+    print("autoupdate disabled")
+
+main.run(update)
