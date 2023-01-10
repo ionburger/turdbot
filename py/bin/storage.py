d4f7e17b1a895437c68193cb4fa7b912f8a98a01
ionburger <ionburgger@gmail.com>
2023-01-09 17:49:22 -0700

    fixed shit

--- py/bin/storage.py
+++ py/bin/storage.py
@@ -8,14 +8,14 @@ class Config:
         return self.db.find_one({'module': module})[key]
 
     def write(self, module, key, value):
-        self.db.update_one({'module': module}, {'$set': {key: value}}, upsert=True)
+        self.db.update_one({'module': str(module)}, {'$set': {str(key): str(value)}}, upsert=True)
         return True
     
     def w(self, module, key, value):
-        return self.write(module, key, value)
+        return self.write(module,key,value)
 
     def r(self, module, key):
-        return self.read(module, key)
+        return self.read(module,key)
 
     def updateguild(self):
         print("Updating guild")
