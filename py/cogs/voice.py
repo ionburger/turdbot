262aac65fa2a5b98c2a550b2c58a5ad3836ed548
ionburger <ionburgger@gmail.com>
2023-01-29 10:10:32 -0700

    ggf

--- py/cogs/voice.py
+++ py/cogs/voice.py
@@ -6,16 +6,22 @@ from bin.storage import Config
 class Voice(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.queue = []
 
-    def player(self, ctx):
-        print(self.queue)
-        if len(self.queue) == 0:
-            return
-        self.queue.pop(0)
-        ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-        ctx.voice_client.play(discord.FFmpegPCMAudio(self.queue[0], **ffmpeg_options), after=Voice.player(self, ctx))
-        
+    def qhandler(self, ctx, error=None):
+        print(ctx.guild.id)
+        st = Config(ctx.guild.id, self.bot.db)
+        queue = st.read("voice", "queue").split("/./")
+        queue.pop(0)
+        st.write("voice", "queue", queue.join("/./"))
+        ffmpeg_options = {
+            'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+            'options': '-vn'
+            }
+        if len(queue) > 0:
+            ctx.voice_client.play(discord.FFmpegPCMAudio(queue[0].get("url",None), **ffmpeg_options), after=self.qhandler(self, ctx))
+            ctx.respond(f"Now playing: {queue[0].get('title',None)}")
+        else:
+            ctx.voice_client.stop()
 
     @commands.command()
     async def join(self, ctx):
@@ -33,12 +39,22 @@ class Voice(commands.Cog):
             await ctx.voice_client.disconnect()
     
     @commands.command()
-    async def play(self, ctx, link):
+    async def play(self, ctx, link=""):
+        if ctx.author.voice is None and channel == "":
+            await ctx.respond("You are not in a voice channel, to specify a channel use `play <link> <channel>`")
+            return
         if ctx.author.voice is None:
-            await ctx.respond("You are not in a voice channel")
+            channel = ctx.guild.get_channel(channel)
+            await channel.connect()
         if ctx.voice_client is None:
             channel = ctx.author.voice.channel
             await channel.connect()
+        if link == "":
+            ctx.voice_client.resume()
+            return
+        if ctx.voice_client.is_paused():
+            ctx.voice_client.resume()
+
         ffmpeg_options = {
             'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
             'options': '-vn'
@@ -57,14 +73,33 @@ class Voice(commands.Cog):
             'source_address': '0.0.0.0',
             }
         with YoutubeDL(ytdl_format_options) as ydl:
-            info_dict = ydl.extract_info(f"ytsearch:{link}", download=False)["entries"][0]
-            video_url = info_dict.get("url", None)
-            video_id = info_dict.get("id", None)
-            video_title = info_dict.get('title', None)
-            self.queue.append(video_url)
+            if link.startswith("https://"):
+                info_dict = ydl.extract_info(link, download=False)
+                video_url = info_dict.get("url", None)
+                video_id = info_dict.get("id", None)
+                video_title = info_dict.get('title', None)
+            else:
+                info_dict = ydl.extract_info(f"ytsearch:{link}", download=False)["entries"][0]
+                video_url = info_dict.get("url", None)
+                video_id = info_dict.get("id", None)
+                video_title = info_dict.get('title', None)
         if not ctx.voice_client.is_playing():
-            ctx.voice_client.play(discord.FFmpegPCMAudio(self.queue[0], **ffmpeg_options), after=Voice.player(self, ctx))
+            ctx.voice_client.play(discord.FFmpegPCMAudio(video_url, **ffmpeg_options), after=self.qhandler(self, ctx))
+            await ctx.respond(f"Now playing: {video_title}")
+        else:
+            st = Config(ctx.guild.id, self.bot.db)
+            queue = st.read("voice", "queue").split("/./")
+            queue.append(info_dict)
+            st.write("voice", "queue", queue.join("/./"))
         
-
+    @commands.command()
+    async def pause(self, ctx):
+        if ctx.voice_client is None:
+            await ctx.respond("I am not in a voice channel")
+        else:
+            ctx.voice_client.pause()
+            await ctx.respond("Paused")
 def setup(bot):
-    bot.add_cog(Voice(bot))
+    bot.add_cog(Voice(bot))        
+
+    
