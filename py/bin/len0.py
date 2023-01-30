87759bd08f0f5bf24c51b325f043869c037830ce
ionburger <ionburgger@gmail.com>
2023-01-30 12:02:42 -0700

    yes

--- py/bin/len0.py
+++
@@ -1,8 +0,0 @@
-# length func which works better for indexing lists
-def len0(args):
-    a = len(args)
-    if a > 0:
-        return a-1
-    else:
-        return False
-
--- py/cogs/voice.py
+++ py/cogs/voice.py
@@ -1,5 +1,5 @@
 import discord
-from discord.ext import commands
+from discord.ext import bridge, commands
 from yt_dlp import YoutubeDL
 from bin.storage import Config
 
@@ -8,42 +8,56 @@ class Voice(commands.Cog):
         self.bot = bot
 
     def qhandler(error=None,self=None, ctx=None, st=None):
-        queue = st.read("voice", "queue").split("/./")
-        queue.pop(0)
-        st.write("voice", "queue", queue.join("/./"))
+        try:
+            queue = (st.read("voice", "queue")).split("/./")
+            queue.pop(0)
+            st.write("voice", "queue", "/./".join(queue))
+        except ValueError:
+            ctx.voice_client.stop()
+            return
         ffmpeg_options = {
             'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
             'options': '-vn'
             }
-        if len(queue) > 0:
-            ctx.voice_client.play(discord.FFmpegPCMAudio(queue[0].get("url",None), **ffmpeg_options), after=self.qhandler(self, ctx))
-            ctx.respond(f"Now playing: {queue[0].get('title',None)}")
-        else:
-            ctx.voice_client.stop()
+        ctx.voice_client.play(discord.FFmpegPCMAudio(queue[0].get("url",None), **ffmpeg_options), after=self.qhandler(self, ctx))
+        ctx.respond(f"Now playing: {queue[0].get('title',None)}")
 
-    @commands.command()
+    @bridge.bridge_command()
     async def join(self, ctx):
+        await ctx.defer()
         if ctx.author.voice is None:
             await ctx.respond("You are not in a voice channel")
         else:
             channel = ctx.author.voice.channel
             await channel.connect()
         
-    @commands.command()
+    @bridge.bridge_command()
     async def leave(self, ctx):
+        await ctx.defer()
+        st = Config(ctx.guild.id, self.bot.db)
+        st.write("voice", "queue", "")
         if ctx.voice_client is None:
             await ctx.respond("I am not in a voice channel")
         else:
             await ctx.voice_client.disconnect()
     
-    @commands.command()
-    async def play(self, ctx, link=""):
-        if ctx.author.voice is None and channel == "":
-            await ctx.respond("You are not in a voice channel, to specify a channel use `play <link> <channel>`")
+    @bridge.bridge_command()
+    async def play(self, ctx, *, video: str):
+        await ctx.defer()
+        args = video.split(" ")
+        channel = ""
+        for arg in range(len(args)-1):
+            if args[arg] == "-channel" or args[arg] == "-c":
+                channel = discord.utils.get(ctx.guild.channels, name=args.pop(arg+1)).id
+                args.pop(arg)
+                break
+        if channel != "" and ctx.author.guild_permissions.administrator == False:
+            await ctx.respond("You do not have permission to specify a channel")
             return
-        if ctx.author.voice is None:
-            channel = ctx.guild.get_channel(channel)
-            await channel.connect()
+        link = " ".join(args)
+        if ctx.author.voice is None and channel == "":
+            await ctx.respond("You are not in a voice channel, to specify a channel use `play <link> -channel <channel>`")
+            return
         if ctx.voice_client is None:
             channel = ctx.author.voice.channel
             await channel.connect()
@@ -88,15 +102,26 @@ class Voice(commands.Cog):
         else:
             queue = st.read("voice", "queue").split("/./")
             queue.append(info_dict)
-            st.write("voice", "queue", queue.join("/./"))
+            st.write("voice", "queue", "/./".join(queue))
         
-    @commands.command()
+    @bridge.bridge_command(alias=["stop"])
     async def pause(self, ctx):
+        await ctx.defer()
         if ctx.voice_client is None:
             await ctx.respond("I am not in a voice channel")
         else:
             ctx.voice_client.pause()
             await ctx.respond("Paused")
+
+    @bridge.bridge_command()
+    async def skip(self, ctx):
+        await ctx.defer()
+        if ctx.voice_client is None:
+            await ctx.respond("I am not in a voice channel")
+        else:
+            ctx.voice_client.stop()
+            await ctx.respond("Skipped")
+
 def setup(bot):
     bot.add_cog(Voice(bot))        
 
