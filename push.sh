caaedc11db34127bb8ed01a4ac2db358edf8d420
Ian Burgess <ionburgger@gmail.com>
2024-06-12 21:32:55 -0600

    added countring setcount

--- push.sh
+++ push.sh
@@ -1,2 +1,2 @@
 docker build -t turdbot -f bot/Dockerfile . && docker push ionburger/turdbot
-docker build -t turdweb -f flask/Dockerfile . && docker push ionburger/turdweb
+#docker build -t turdweb -f flask/Dockerfile . && docker push ionburger/turdweb
