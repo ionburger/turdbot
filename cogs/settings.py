ac7ff93e782c1a447b26bfb92e75c8f117f66a48
John Doe <johndoe@example.com>
2024-10-02 15:18:33 -0600

    fixes

--- cogs/settings.py
+++ cogs/settings.py
@@ -8,11 +8,6 @@ class Settings(commands.Cog):
     @bridge.bridge_group(aliases=["set", "s"], invoke_without_command=True)
     async def settings(self, ctx):
         await ctx.respond("invalid command, see !help settings for more info")
-    
-    @settings.command()
-    
-
-
 
 def setup(bot):
     bot.add_cog(Settings(bot))
